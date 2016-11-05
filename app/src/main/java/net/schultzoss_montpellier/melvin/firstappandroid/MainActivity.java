b6d66c32cffe695ea4bb7348dc1f6d41d8a1688f
melvinschultz <melvinschultzdev@gmail.com>
2016-11-05 11:23:18 +0100

    display the message in other Activity

--- app/src/main/java/net/schultzoss_montpellier/melvin/firstappandroid/MainActivity.java
+++ app/src/main/java/net/schultzoss_montpellier/melvin/firstappandroid/MainActivity.java
@@ -1,13 +1,30 @@
 package net.schultzoss_montpellier.melvin.firstappandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the send button */
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
